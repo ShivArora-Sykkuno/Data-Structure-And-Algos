d1af6613a554cd806047ce9611396582dc189ec6
SHIV ARORA <shivarora0069@gmail.com>
2025-02-25 07:52:59 +0530

    Starters 133

---
+++ CodeChef/ABCC3.py
@@ -0,0 +1,25 @@
+# cook your dish here
+
+for _ in range(int(input())):
+    num = int(input())
+    str = input()
+
+    a, b = [0 for _ in range(num)], []
+    for i in range(num):
+        a[i] = a[i - 1] + (1 if str[i] == 'a' else 0)
+        if (str[i] == 'b'):
+            b.append(i)
+
+    c = [0 for _ in range(num + 1)]
+    for i in range(num - 1, -1, -1):
+        c[i] = c[i + 1] + (1 if str[i] == 'c' else 0)
+
+    # print(a, c)
+    res = float('inf')
+    for i in range(1, len(b)):
+        res = min(res, a[b[i - 1]] + c[b[i]])
+    if (len(b) != 0):
+        res = min(res, c[b[0]], a[b[-1]])
+    else:
+        res = 0
+    print(res)
