256083bf4a380057e09d2af9f87d91bd27a78ef7
SHIV ARORA <shivarora0069@gmail.com>
2025-04-27 16:00:09 +0530

    LeetCode Interview Question

--- LeetCode/Problem 70.py
+++ LeetCode/Problem 70.py
@@ -1,5 +1,12 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 0 or n == 1:
-            return 1
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        if n <= 2:
+            return n
+
+        dp = [0] * (n + 1)
+        dp[1], dp[2] = 1, 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
