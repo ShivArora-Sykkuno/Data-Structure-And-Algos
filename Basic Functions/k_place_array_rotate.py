355bc7658f56a39d9f55124676778d6c49d7d09b
SHIV ARORA <shivarora0069@gmail.com>
2025-07-10 05:13:00 +0530

    Basic Functions

--- Basic Functions/k_place_array_rotate.py
+++ Basic Functions/k_place_array_rotate.py
@@ -1,8 +1,14 @@
+#brute Force approach
 def array_rotate(arr, k):
     for i in range(k):
         arr[:] = [arr[-1]] + arr[ : len(arr)-1]
     return arr
 
+# Optimized approach
+def optimal_array_rotate(arr, k):
+    rot = k % len(arr) #to handle cases where k is greater than the length of the array
+    pos = len(arr) - rot # Find the split position
+    return arr[pos:] + arr[:pos]
 
 a = [3,9,5,6,7,2]
 print(array_rotate(a, 3))
