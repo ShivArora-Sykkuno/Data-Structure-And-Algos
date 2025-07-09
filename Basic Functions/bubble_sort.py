59c3e22a12c7eaaa75d811620fc8593ea6063ac4
SHIV ARORA <shivarora0069@gmail.com>
2025-07-09 08:14:11 +0530

    Basic Functions

--- Basic Functions/bubble_sort.py
+++ Basic Functions/bubble_sort.py
@@ -1,10 +1,12 @@
 def bubble_sort(arr):
-    if len (arr) == 0:
-        return 0
     for i in range(len(arr)):
+        swap = False
         for j in range(len(arr)-i-1):
             if arr[j]> arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j] #?Swapping     
+                arr[j], arr[j+1] = arr[j+1], arr[j] #?Swapping
+                swap = True
+        if swap == False:
+            break     
     return arr
 
 a = [2,6,8,5,3,1]
