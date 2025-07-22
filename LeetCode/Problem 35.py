6481c9f1fee9ecde9fff84bfc83991e7d2e8e0e7
SHIV ARORA <shivarora0069@gmail.com>
2025-07-22 18:19:16 +0530

    LeetCode Interview Question

--- LeetCode/Problem 35.py
+++ LeetCode/Problem 35.py
@@ -1,7 +1,13 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-        for i in range(len(nums)):
-            if nums[i] == target or nums[i]> target:
-                return i 
-
-        return len(nums)  
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        ub = len(nums)
+        low, high = 0, len(nums)-1
+        while high >= low:
+            mid = (low + high) // 2
+            if nums[mid] >= target:
+                ub = mid
+                high = mid - 1
+            elif  nums[mid] < target:
+                low = mid +1
+            
+        return ub
