66e6cd10fafdfe2b8d102388c16f9216e9714fe2
SHIV ARORA <shivarora0069@gmail.com>
2025-08-17 16:05:26 +0530

    GFG Practice Question

---
+++ GFG/Subset Sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def __init__(self):
+        self.ans = []
+        
+    def solve(self, index, total, arr):
+        
+        if index == len(arr):
+            self.ans.append(total)
+            return
+        self.solve(index+1, total+ arr[index], arr) 
+        self.solve(index+1, total, arr)
+        
+	def subsetSums(self, arr):
+	    self.solve(0, 0, arr)
+	    return self.ans
